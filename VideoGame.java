b34739160dcdc5a959a3368ef9b104bf6592b6af
ydeggouj <147187772+ydeggouj@users.noreply.github.com>
2023-10-11 13:21:12 -0400

    Using better alien factory in game
    Using the better alien factory to create aliens

--- VideoGame.java
+++ VideoGame.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class VideoGame {
     private static final int ALIENGANG = 10;
     public void play() {
-        AlienFactory af = new AlienFactory();
+        BetterAlienFactory af = new BetterAlienFactory();
         Alien[] aliens = new Alien[ALIENGANG];
         for (int i = 0; i < aliens.length; i++) {
             aliens[i] = af.getAlien();
